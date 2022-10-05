a66c35f95cd6c28b76eed7a87e0aa504bc00e5dd
HtSpDuyTran <83074825+HtSpDuyTran@users.noreply.github.com>
2022-10-05 15:05:16 -0300

    adding files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.scss';
+
+//custom components
+import AppBar from 'components/AppBar/AppBar'
+import BoardBar from './components/BoardBar/BoardBar';
+import BoardContent from './components/BoardContent/BoardContent';
+
+
+function App() {
+  return (
+    <div className="trello-container">
+        <AppBar/>
+        <BoardBar/>
+        <BoardContent/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Column/Column.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Column.scss'
+
+function Column(){
+    return (
+        <div className='column'>
+        <header>Brainstorm</header>
+       <ul>
+         <li>
+           <img src='https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-1/271180999_6715074841898050_3119676219567720605_n.jpg?stp=dst-jpg_p320x320&_nc_cat=102&ccb=1-7&_nc_sid=7206a8&_nc_ohc=P3PCTsYPY3gAX-BEnAT&_nc_ht=scontent-lga3-2.xx&oh=00_AT_ErSWuf9qUEUicjHgo2rVGTI-2bxdMSaQTeCFKHBDxOA&oe=63407127' alt='duy-img'></img>
+           Title: Duy Tran</li>
+         <li>Testing ksacmsakadcascascascascascascm</li>
+         <li>Testing ksacmsakm</li>
+         <li>Testing ksacmsakm</li>
+         <li>Testing ksacmsakm</li>
+       </ul>
+       <footer>Add another cards</footer>
+     </div>
+    
+    )
+ 
+}
+
+export default Column
